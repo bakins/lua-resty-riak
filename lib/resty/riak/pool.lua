abbd3c9185e1d98b00903af2a9f154741c9f621e
Brian Akins <brian.akins@turner.com>
2012-08-06 20:19:02 -0400

    playing with metatables

--- lib/resty/riak/pool.lua
+++ lib/resty/riak/pool.lua
@@ -1,48 +1,81 @@
 local _M = {}
 
-_M._VERSION = '0.0.1'
-
--- https://wiki.basho.com/PBC-API.html
--- https://github.com/basho/riak_pb
-
--- this is insired by the riak ruby client
-
 local mt = {}
 
-local client = require "nginx.riak.client"
+local riak = require "resty.riak"
 
 local insert = table.insert
+
+local function connect(self)
+    local servers = self.servers
+    local ok, err
+    for i=1,self.retries do
+        local curr = mod(self.riak._current_server + 1, #servers) + 1
+        self._current_server = curr
+        local server = servers[curr]
+
+        self.riak = riak.new()
+        if self.timeout then
+            self.riak:set_timeout(self.timeout)
+        end
+        ok, err = self.riak:connect(server.host, server.port)
+        if ok then
+            self.open = true
+            setmetatable(self, { __index = self.riak })
+            break
+        end
+    end
+    return ok, err
+end
+
+local function close(self, really_close)
+    if not self.riak then
+        return nil, "not initialized"
+    end
+    if not self.open then
+        return nil, "closed"
+    end
+    self.open = false
+    if really_close or self.really_close then
+        return self.riak:close()
+    else
+        if self.keepalive_timeout or self.keepalive_pool_size then
+            return self.riak:set_keepalive(self.keepalive_timeout, self.keepalive_pool_size)
+        else
+            return self.riak:set_keepalive()
+        end
+    end
+end
 
 -- servers should be in the form { {:host => host/ip, :port => :port }
 function _M.new(servers, options)
     options = options or {}
-    local r = {
+    local self = {
         servers = {},
         _current_server = 1,
         timeout = options.timeout,
         keepalive_timeout = options.keepalive_timeout,
         keepalive_pool_size = options.keepalive_pool_size,
         really_close = options.really_close,
-        retries = options.retries or 3
+        retries = options.retries or 1,
+        open = false
     }
-    if r.retries <= 0 then
-        r.retries = 1
+    if self.retries <= 0 then
+        self.retries = 1
     end
     servers = servers or {{ host = "127.0.0.1", port = 8087 }}
     for _,server in ipairs(servers) do
         if "table" == type(server) then
-            insert(r.servers, { host = server.host or "127.0.0.1", port = server.port or 8087 })
+            insert(self.servers, { host = server.host or "127.0.0.1", port = server.port or 8087 })
         else
-            insert(r.servers, { host = server, port = 8087 })
+            insert(self.servers, { host = server, port = 8087 })
         end
     end
     
-    setmetatable(r, { __index = mt })
+    self.connect = connect
+    self.close = close
+    --setmetatable(r, { __index = mt })
     return r
-end
-
-function mt.connect(self)
-    return client.connect(self)
 end
 
 return _M
