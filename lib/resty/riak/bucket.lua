055da763b06de9b12203b25b240a4f199ee9d072
Brian Akins <brian.akins@turner.com>
2013-01-04 09:46:43 -0500

    use object metatable

--- lib/resty/riak/bucket.lua
+++ lib/resty/riak/bucket.lua
@@ -2,39 +2,32 @@ local _M = {}
 
 local robject = require "resty.riak.object"
 
-local mt = { 
-    __index = _M 
-}
+local mt = { }
 
 function _M.new(client, name)
-    -- cheesy...
     local self = {
         name = name, 
-        client = client,
-        get = _M.get_object,
-        new = _M.new_object,
-        get_or_new = _M.get_or_new_object,
-        delete = _M.delete_object
+        client = client
     }
-    return setmetatable(self, mt)
+    return setmetatable(self, { __index = mt })
 end
 
-function _M.get_object(self, key)
-    return self.client.get_object(self, key)
+function mt.get(self, key)
+    return self.client:get_object(self, key)
 end
 
-function _M.new_object(self, key)
+function mt.new(self, key)
     return robject.new(self, key)
 end
 
-function _M.get_or_new_object(self, key)
-    local object = self.client.get_object(self, key)
+function mt.get_or_new(self, key)
+    local object = self.client:get_object(self, key)
     if not object then
         return robject.new(self, key)
     end
 end
 
-function _M.delete_object(self, key)
+function mt.delete(self, key)
     return self.client:delete_object(self, key)
 end
 
