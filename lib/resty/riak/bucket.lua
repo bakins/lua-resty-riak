034e21fdba2d223204b384a55bb94a3bb19b5cb3
bakins <brian.akins@turner.com>
2013-03-01 20:42:34 -0500

    just pass name of bucket, not bueck object

--- lib/resty/riak/bucket.lua
+++ lib/resty/riak/bucket.lua
@@ -13,22 +13,22 @@ function _M.new(client, name)
 end
 
 function mt.get(self, key)
-    return self.client:get_object(self, key)
+    return self.client:get_object(self.name, key)
 end
 
 function mt.new(self, key)
-    return robject.new(self, key)
+    return robject.new(self.name, key)
 end
 
 function mt.get_or_new(self, key)
-    local object = self.client:get_object(self, key)
+    local object = self.client:get_object(self.name, key)
     if not object then
-        return robject.new(self, key)
+        return robject.new(self.name, key)
     end
 end
 
 function mt.delete(self, key)
-    return self.client:delete_object(self, key)
+    return self.client:delete_object(self.name, key)
 end
 
 return _M
