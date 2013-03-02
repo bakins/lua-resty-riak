4e413ac471b76f511490ff1a658f0d7640f98748
bakins <brian.akins@turner.com>
2013-03-01 22:02:52 -0500

    use new raw client

--- lib/resty/riak/bucket.lua
+++ lib/resty/riak/bucket.lua
@@ -1,34 +1,51 @@
 local _M = {}
 
-local robject = require "resty.riak.object"
+local riak_object = require "resty.riak.object"
+local riak_client = require "resty.riak.client"
 
-local mt = { }
+local riak_object_new = riak_object.new
+local function new(self, key)
+    return riak_object_new(self, key)
+end
 
 function _M.new(client, name)
     local self = {
         name = name, 
-        client = client
+        client = client, 
+	new = new
     }
-    return setmetatable(self, { __index = mt })
+    return setmetatable(self, { __index = _M })
 end
 
-function mt.get(self, key)
-    return self.client:get_object(self.name, key)
-end
+local riak_client_get_object = riak_client.get_object
+local riak_object_load = riak_object.load
 
-function mt.new(self, key)
-    return robject.new(self.name, key)
-end
-
-function mt.get_or_new(self, key)
-    local object = self.client:get_object(self.name, key)
-    if not object then
-        return robject.new(self.name, key)
+function _M.get(self, key)
+    local object, err = riak_client_get_object(self.client, self.name, key)
+    if object then
+	return riak_object_load(self, key, object)
+    else
+	return nil, err
     end
 end
 
-function mt.delete(self, key)
-    return self.client:delete_object(self.name, key)
+function _M.get_or_new(self, key)
+    local object, err = riak_client_get_object(self.client, self.name, key)
+    if not object then
+	if "not found" == err then
+	    return riak_object_new(self, key)
+	else
+	    return nil, err
+	end
+    else
+	return riak_object_load(object)
+    end
+end
+
+local riak_client_delete_object = riak_client.delete_object
+
+function _M.delete(self, key)
+    return riak_client_delete_object(self.client, self.name, key)
 end
 
 return _M
