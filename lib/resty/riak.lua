fbfd118b8e9d79220d8a2de657039754412163b4
bakins <brian.akins@turner.com>
2013-03-01 21:59:57 -0500

    'inherit' from riak client

--- lib/resty/riak.lua
+++ lib/resty/riak.lua
@@ -1,7 +1,16 @@
 local _M = {}
-_M._VERSION = '0.1.0'
+_M._VERSION = '0.2.0'
 
-local client = require("resty.riak.client")
-_M.new = client.new
+local riak_bucket = require "resty.riak.bucket"
+local riak_bucket_new = riak_bucket.new
+
+local riak_client = require "resty.riak.client"
+local riak_client_new = riak_client.new
+
+function _M.new()
+    local self = riak_client_new()
+    self.bucket = riak_bucket_new
+    return self
+end
 
 return _M
