d1ba6768071e6f2d2eabe77846af508d1d3bcff9
bakins <brian.akins@turner.com>
2013-04-05 22:00:43 -0400

    add ldoc

--- lib/resty/riak/helpers.lua
+++ lib/resty/riak/helpers.lua
@@ -1,3 +1,6 @@
+--- Internal helpers for resty
+-- @module resty.riak.helpers
+
 local _M = {}
 
 -- based on strict.lua and agentzh's lua-resty-*
@@ -5,21 +8,23 @@ local _M = {}
 local getinfo = debug.getinfo
 
 local function what ()
-  local d = getinfo(3, "S")
-  return d and d.what or "C"
+    local d = getinfo(3, "S")
+    return d and d.what or "C"
 end
 
 local class_mt = {
     -- to prevent use of casual module global variables
     __newindex = function(table, key, val)
-		     local w = what()
+	local w = what()
 		     if w ~= "main" and w ~= "C" then
 			 error('attempt to write to undeclared variable "' .. key .. '"')
 		     end
 		     rawset(table, key, val)
-		 end
+    end
 }
 
+--- create a "module". This sets up metatable to avoid using undeclared globals, etc
+-- @treturn table the module
 function _M.module()
     local _M = {}
     setfenv(2, _M)
