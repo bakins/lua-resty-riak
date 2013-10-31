1ec691b60affe6cde56a64d8234a354080d75728
bakins <brian.akins@turner.com>
2013-10-30 21:02:16 -0400

    metatable abstraction

--- lib/resty/riak/object.lua
+++ lib/resty/riak/object.lua
@@ -12,10 +12,51 @@ local helpers = require("resty.riak.helpers")
 local RpbPairs_to_table = helpers.RpbPairs_to_table
 local table_to_RpbPairs = helpers.table_to_RpbPairs
 local ipairs = ipairs
+local rawset = rawset
+local rawget = rawget
 
 local _M = helpers.module()
 
 local riak_client = require "resty.riak.client"
+
+--- fields on the content can be set by shorthand helpers. These only work on the first
+-- sibling if more than one is present
+-- @field value
+-- @field charset
+-- @field content_encoding
+-- @field content_type
+-- @table new_index_fields
+local new_index_fields = {}
+for i,key in ipairs({"value", "charset", "content_encoding", "content_type" }) do
+    new_index_fields[key] = true
+end
+
+local function newindex(self, k, v)
+    if new_index_fields[k] then
+	rawset(self.siblings[1], k, v)
+    else
+	error("invalid field: " .. k)
+    end
+end
+
+local index_fields = {}
+for i,key in ipairs({"value", "charset", "content_encoding", "content_type", "last_mod", "meta"}) do
+    index_fields[key] = true
+end
+
+local function index(self, k)
+    if index_fields[k] then
+	local content = self.siblings[1]
+	return content[k]
+    else
+	return _M[k]
+    end
+end
+
+local mt = {
+    __index = index,
+    __newindex = newindex
+}
 
 --- Create a new riak object. This does not change anything in riak, it only sets up a Lua object.
 -- This does **not** persist to the server(s) until @{store} is called. Generally, @{resty.riak.bucket.new_object}
@@ -28,11 +69,14 @@ function _M.new(bucket, key)
         bucket = bucket,
 	client = bucket.client,
         key = key,
-        meta = {},
-	indexes = {},
-	siblings = {}
+	siblings = {
+	    {
+		meta = {},
+		indexes = {}
+	    }
+	}
     }
-    return setmetatable(o,  { __index = _M })
+    return setmetatable(o, mt)
 end
 
 --- Create a "high level" object from a table returned by @{resty.riak.client.get_object}. This is considered a "private" function
@@ -65,7 +109,7 @@ function _M.load(bucket, key, response)
 	siblings = siblings
     }
 
-    return setmetatable(object, { __index = _M })
+    return setmetatable(object, mt)
 end
 
 function _M.content(self)
@@ -76,24 +120,12 @@ function _M.has_siblings(Self)
     return self.siblings[1] ~= nil
 end
 
-for i,key in ipairs({"value", "charset", "content_encoding", "content_type", "last_mod", "meta"}) do
-    _M[key] = function(self, sibling)
-	sibling = sibling or 1
-	local content = self.siblings[sibling]
-	if content then
-	    return content[key]
-	else
-	    return nil
-	end
-    end
-end
-
 local riak_client_store_object = riak_client.store_object
 --- Persist an object to riak.
 -- @tparam resty.riak.object self
 -- @see resty.riak.client.store_object
 function _M.store(self)
-    local content = self.siblings and self.siblings[1] or self.content
+    local content = self.siblings[1]
 
     local object = {
         key = self.key,
